2576619547dc881ffebbf8a7cad7f6f40be33429
Kyoshim <chaugne.christophe@gmail.com>
2015-06-05 16:03:24 +0200

    add app

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var cpu = require('./lib/cpu'),
+		drive = require('./lib/drive'),
+		http = require('http');
+
+if (process.platform == 'linux')
+	var netstat = require('./lib/netstat'),
+			security = require('./lib/security'),
+			mem = require('./lib/mem'),
+  		proc = require('./lib/proc');
+
+http.createServer(function(req, res) {
+  res.end('Thanks');
+}).listen(5005);
