a5e9cfc0c93e419b7768822522fb4c0dd1ddd6f1
Kyoshim <chaugne.christophe@gmail.com>
2015-07-01 18:22:03 +0200

    Add remotes actions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cpu = require('./lib/cpu'),
     users = require('./lib/users'),
     pmx = require('pmx'),
     pm2 = require('pm2'),
+    shelljs = require('shelljs'),
     fs      = require('fs'),
     path    = require('path');
 
@@ -46,3 +47,68 @@ var conf = pmx.initModule({
     // Green / Yellow / Red
   }
 });
+
+pmx.action('vmstats', function(reply) {
+  var vmstats = shelljs.exec('vmstat -S m', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('processes/users', function(reply) {
+  var proc_users = shelljs.exec('ps hax -o user | sort | uniq -c', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('disk usage', function(reply) {
+  var disk_usage = shelljs.exec('df -h', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('who', function(reply) {
+  var who = shelljs.exec('who', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('uptime', function(reply) {
+  var uptime = shelljs.exec('uptime', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('top cpu consuming', function(reply) {
+  var top_cpu_process = shelljs.exec('ps -eo pcpu,user,args --no-headers | sort -k 1 -nr | head -n 10 | cut -c 1-70', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('top mem consuming', function(reply) {
+  var top_mem_process = shelljs.exec('ps -eo pmem,pid,cmd | sort -k 1 -nr | head -10 | cut -c 1-70', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('open ports', function(reply) {
+  var open_ports = shelljs.exec('lsof -Pni4 | grep ESTABLISHED', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "<br />");
+    return reply(result);
+  });
+});
+
+pmx.action('open files', function(reply) {
+  var open_files = shelljs.exec('lsof -i -n -P | wc -l', { async : true, silent:true}, function(err, out) {
+    var result = out.replace(/\n/g, "");
+    result = parseInt(result) - 1;
+    return reply(result);
+  });
+});
+
